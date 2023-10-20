27e3381604309c3a2c00216981c0de63074ce5fc
Syphonphilter <itsbalaabdul31@yahoo.com>
2023-10-20 05:14:52 -0300

    Back Space Compare, a solution that removes chars(y) before a given char(x) version 2 with stacks

--- C-Sharp-Playground/Code/BackspaceCompare.cs
+++ C-Sharp-Playground/Code/BackspaceCompare.cs
@@ -20,4 +20,33 @@
      return s==t;
                 
       }
+
+       static bool BackspaceCompare_v2(string s, string t)
+        {
+        Stack<string> s1 = new Stack<string>();
+        Stack<string> s2 = new Stack<string>();
+        foreach(char x in s){
+            if(x=='#'&& s1.Count>0){
+            s1.Pop();
+            }
+            else {
+            s1.Push(x.ToString());
+            }
+            
+        }
+        foreach(char x in t){
+            if(x=='#'&& s2.Count>0){
+            s2.Pop();
+            }
+            else {
+            s2.Push(x.ToString());
+            }
+            
+        }
+        
+        
+        return s1.SequenceEqual(s2);
+                    
+        }
+      
  }
--- C-Sharp-Playground/Program.cs
+++ C-Sharp-Playground/Program.cs
@@ -9,29 +9,9 @@ class Program
  
      static void Main()
     {
-     Console.WriteLine(b2("abc##def","ab#def"));
+     
     }
-    static bool b2(string s, string t)
-    {
-      
-      if(s.Length<1&&t.Length>200){
-        return false;
-      }
-  
-      do{
-          if(s.Contains('#') || t.Contains('#')){
-          Console.WriteLine(s.IndexOf('#'));
-          s = new string(s.TakeWhile((c,i)=>  i<s.IndexOf('#')-1).ToArray())+s.Substring(s.IndexOf("#") + "#".Length);
-          t = new string(t.TakeWhile((c,i)=>  i<t.IndexOf('#')-1).ToArray())+t.Substring(t.IndexOf("#") + "#".Length);
-          
-          }
-          
-      }while(s.Contains('#') || t.Contains('#'));
-      
-     return s==t;
-                
-      }
-      
+   
     
    
 }
