f63640f1ca2eef5443ae0f389844299969c4d401
Syphonphilter <itsbalaabdul31@yahoo.com>
2023-10-24 21:00:43 -0300

    A Search Insert Position solution

--- C-Sharp-Playground/Code/SearchInsertPosition.cs
+++ C-Sharp-Playground/Code/SearchInsertPosition.cs
@@ -2,6 +2,16 @@ namespace C_Sharp_Playground.Code
 {
     public class SearchInsertPosition
     {
-        
+         public static int  searchInsertPosition(int[] nums,int  target)
+    {
+        int index  = Array.IndexOf(nums,target);
+        int minDistance = (from c in nums where target-c>0 select c).DefaultIfEmpty().Max();
+        int minDistanceIndex  = EqualityComparer<int>.Default.Equals(minDistance, 0)?0: Array.IndexOf(nums,minDistance)+1;
+       return index switch{
+           -1=>minDistanceIndex,
+           >=0=>index
+        };
+      
+    }
     }
 }
