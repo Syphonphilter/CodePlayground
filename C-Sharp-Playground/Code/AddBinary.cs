972178fca9bf5673d3307465d258a1264ff32600
Syphonphilter <itsbalaabdul31@yahoo.com>
2023-10-31 10:34:11 -0300

    Add Binary of two values (a,b) where 1 <= a.length, b.length <= 10^4

--- C-Sharp-Playground/Code/AddBinary.cs
+++ C-Sharp-Playground/Code/AddBinary.cs
@@ -2,6 +2,12 @@ namespace C_Sharp_Playground.Code
 {
     public class AddBinary
     {
-        public string addBinary(string a, string b)
+        public static string AaddBinary(string a, string b)
+        {
+            UInt128 _a = UInt128.Parse(a, System.Globalization.NumberStyles.AllowHexSpecifier);
+            UInt128 _b = UInt128.Parse(b, System.Globalization.NumberStyles.AllowHexSpecifier);
+            UInt128 sum = _a + _b;
+            return Convert.ToString((byte)sum, 2);
+        }
     }
 }
--- C-Sharp-Playground/Program.cs
+++ C-Sharp-Playground/Program.cs
@@ -15,21 +15,14 @@ class Program
 
     static void Main()
     {
-        int[] d = { 7,2,8,5,0,9,1,2,9,5,3,6,6,7,3,2,8,4,3,7,9,5,7,7,4,7,4,9,4,7,0,1,1,1,7,4,0,0,6 };
-        PlusOne(d);
+        AddBinary("1110001", "110100100");
 
     }
-    public static int[] PlusOne(int[] digits)
+    public static string AddBinary(string a, string b)
     {
-        var concatDigits = BigInteger.Parse(string.Join("", digits)) + 1;
-        string counter = concatDigits.ToString();
-
-        digits = counter.Length > digits.Length ? new int[counter.Length] : digits;
-        for (int i = 0; i <= counter.Length - 1; i++)
-        {
-            Console.WriteLine(counter[i]);
-            digits[i] = int.Parse(counter[i].ToString());
-        }
-        return digits;
+        UInt128 _a = UInt128.Parse(a, System.Globalization.NumberStyles.AllowHexSpecifier);
+        UInt128 _b = UInt128.Parse(b, System.Globalization.NumberStyles.AllowHexSpecifier);
+        UInt128 sum = _a + _b;
+        return Convert.ToString((byte)sum, 2);
     }
 }
