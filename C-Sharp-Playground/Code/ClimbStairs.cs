356612c5eddf34d39dd8e5560bb82eca8e32798c
syphonphilter <itsbalaabdul31@yahoo.com>
2023-11-05 21:18:28 -0400

    a program that uses fibbonaci sequence to climb n steps 1 or two times and how many distict steps you can take

--- C-Sharp-Playground/Code/ClimbStairs.cs
+++ C-Sharp-Playground/Code/ClimbStairs.cs
@@ -2,6 +2,24 @@ namespace C_Sharp_Playground.Code
 {
     public class ClimbStairs
     {
+        /// <summary>
+        /// Calculates the number of distinct ways to climb to the top of a staircase with the given number of steps.
+        /// </summary>
+        /// <param name="num">The number of steps in the staircase.</param>
+        /// <returns>The number of distinct ways to climb to the top of the staircase.</returns>
+        public static int ClimbStairs_(int num)
+        {
+            int[] dp = new int[num + 1];
+            dp[0] = 1;
+            dp[1] = 1;
 
+            for (int i = 2; i <= num; i++)
+            {
+                dp[i] = dp[i - 1] + dp[i - 2];
+            }
+
+            return dp[num];
+        }
+        /// End Code Snippet ///
     }
 }
--- C-Sharp-Playground/Program.cs
+++ C-Sharp-Playground/Program.cs
@@ -15,11 +15,26 @@ class Program
 
     static void Main()
     {
-        AddBinary("1110001", "110100100");
+        Console.WriteLine(ClimbStairs(44));
 
     }
+    /// <summary>
+    /// Calculates the number of distinct ways to climb to the top of a staircase with the given number of steps.
+    /// </summary>
+    /// <param name="num">The number of steps in the staircase.</param>
+    /// <returns>The number of distinct ways to climb to the top of the staircase.</returns>
     public static int ClimbStairs(int num)
     {
+        int[] dp = new int[num + 1];
+        dp[0] = 1;
+        dp[1] = 1;
 
+        for (int i = 2; i <= num; i++)
+        {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[num];
     }
+    /// End Code Snippet ///
 }
