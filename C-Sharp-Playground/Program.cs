751524436f23670c359dca5b9e2730917a267537
syphonphilter <itsbalaabdul31@yahoo.com>
2023-11-09 23:59:43 -0400

    In-order binary tree transversal fix

--- C-Sharp-Playground/Program.cs
+++ C-Sharp-Playground/Program.cs
@@ -25,22 +25,15 @@ class Program
     {
         // Creating nodes
         TreeNode root = new TreeNode(1);
-        TreeNode node2 = new TreeNode(2);
-        TreeNode node3 = new TreeNode(3);
-        TreeNode node4 = new TreeNode(4);
-        TreeNode node5 = new TreeNode(5);
-        TreeNode node6 = new TreeNode(6);
-        TreeNode node7 = new TreeNode(7);
+        TreeNode node2 = null;
+        TreeNode node3 = new TreeNode(2);
+        TreeNode node4 = new TreeNode(3);
 
         // Building the tree
         root.left = node2;
         root.right = node3;
 
-        node2.left = node4;
-        node2.right = node5;
-
-        node3.left = node6;
-        node3.right = node7;
+        node3.left = node4;
 
         return root;
     }
@@ -61,6 +54,6 @@ class Program
 
         Traverse(root);
 
-        return stack.ToList();
+        return stack.Reverse().ToList();
     }
 }
