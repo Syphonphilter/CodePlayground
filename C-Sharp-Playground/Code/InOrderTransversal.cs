5569b2107b0bd5a0070f7a4a56e6820c027b14c2
syphonphilter <itsbalaabdul31@yahoo.com>
2023-11-10 23:08:34 -0400

    IsSameTree init

--- C-Sharp-Playground/Code/InOrderTransversal.cs
+++ C-Sharp-Playground/Code/InOrderTransversal.cs
@@ -2,6 +2,23 @@ namespace C_Sharp_Playground.Code
 {
     public class InOrderTransversal
     {
-        
+        public static IList<int> InorderTraversal(TreeNode root)
+        {
+            Stack<int> stack = new Stack<int>();
+            void Traverse(TreeNode node)
+            {
+                if (node == null)
+                {
+                    return;
+                }
+                Traverse(node.left);
+                stack.Push(node.val);
+                Traverse(node.right);
+            }
+
+            Traverse(root);
+
+            return stack.Reverse().ToList();
+        }
     }
 }
--- C-Sharp-Playground/Program.cs
+++ C-Sharp-Playground/Program.cs
@@ -9,6 +9,7 @@ using Playground.Code;
 using Xunit.Sdk;
 using System.Numerics;
 using C_Sharp_Playground.Code.Functions;
+using System.Collections;
 
 class Program
 {
@@ -16,18 +17,18 @@ class Program
 
     static void Main()
     {
-        var x = InorderTraversal(CreateSampleTree());
+        var x = IsSameTree(CreateSampleTree(1, 2, 3), CreateSampleTree(1,2,3));
         Console.WriteLine(x);
 
     }
 
-    public static TreeNode CreateSampleTree()
+    public static TreeNode CreateSampleTree(int a, int b, int c)
     {
         // Creating nodes
-        TreeNode root = new TreeNode(1);
+        TreeNode root = new TreeNode(a);
         TreeNode node2 = null;
-        TreeNode node3 = new TreeNode(2);
-        TreeNode node4 = new TreeNode(3);
+        TreeNode node3 = new TreeNode(b);
+        TreeNode node4 = new TreeNode(c);
 
         // Building the tree
         root.left = node2;
@@ -38,6 +39,33 @@ class Program
         return root;
     }
 
+    public static bool IsSameTree(TreeNode p, TreeNode q)
+    {
+
+        if ((p != null && q == null) || (p == null && q != null))
+        {
+            return false;
+        }
+        Stack<int> stack = new Stack<int>();
+        List<int> Traverse(TreeNode node)
+        {
+            if (node == null)
+            {
+                return new List<int>();
+            }
+            Traverse(node.left);
+            stack.Push(node.val);
+            Traverse(node.right);
+            return stack.Reverse().ToList();
+        }
+
+
+        List<int> a = Traverse(p);
+        List<int> b = Traverse(q);
+
+
+        return a == b;
+    }
     public static IList<int> InorderTraversal(TreeNode root)
     {
         Stack<int> stack = new Stack<int>();
@@ -53,7 +81,7 @@ class Program
         }
 
         Traverse(root);
-
+        Console.WriteLine("hre");
         return stack.Reverse().ToList();
     }
 }
