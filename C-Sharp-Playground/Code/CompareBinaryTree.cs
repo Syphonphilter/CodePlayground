fcd55d46645ef434973a0fb5fa68835830aa59c3
syphonphilter <itsbalaabdul31@yahoo.com>
2023-11-11 00:20:13 -0400

    IsSameTree Function

--- C-Sharp-Playground/Code/CompareBinaryTree.cs
+++ C-Sharp-Playground/Code/CompareBinaryTree.cs
@@ -2,6 +2,18 @@ namespace C_Sharp_Playground.Code
 {
     public class CompareBinaryTree
     {
-        
+        public bool IsSameTree(TreeNode p, TreeNode q)
+        {
+            // Base case: if both nodes are null, they are the same
+            if (p == null && q == null)
+                return true;
+
+            // If one node is null and the other is not, they are not the same
+            if (p == null || q == null)
+                return false;
+
+            // Check if the values are the same and recursively check left and right subtrees
+            return p.val == q.val && IsSameTree(p.left, q.left) && IsSameTree(p.right, q.right);
+        }
     }
 }
