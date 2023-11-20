9ed8713d6e2bbd0dca02935aeb3952e296fc6bd5
syphonphilter <itsbalaabdul31@yahoo.com>
2023-11-20 00:15:03 -0400

    Check if BT is balanced v2

--- C-Sharp-Playground/Code/IsBalancedBinaryTree.cs
+++ C-Sharp-Playground/Code/IsBalancedBinaryTree.cs
@@ -8,21 +8,58 @@ namespace C_Sharp_Playground.Code
             {
                 return true;
             }
-            static int GetDept(TreeNode root)
+
+            int GetDepth(TreeNode node)
             {
-                if (root == null)
+                if (node == null)
                 {
                     return 0;
                 }
 
-                int leftDepth = GetDept(root.left);
-                int rightDepth = GetDept(root.right);
+                return Math.Max(GetDepth(node.left), GetDepth(node.right)) + 1;
+            }
+
+            return Math.Abs(GetDepth(root.left) - GetDepth(root.right)) <= 1;
+        }
+
+
+
+
+        public bool IsBalanced2(TreeNode root)
+        {
+            if (root == null)
+            {
+                return true;
+            }
+
+            int GetDepth(TreeNode node)
+            {
+                if (node == null)
+                {
+                    return 0;
+                }
+
+                int leftDepth = GetDepth(node.left);
+                if (leftDepth == -1)
+                {
+                    return -1;
+                }
+
+                int rightDepth = GetDepth(node.right);
+                if (rightDepth == -1)
+                {
+                    return -1;
+                }
+
+                if (Math.Abs(leftDepth - rightDepth) > 1)
+                {
+                    return -1;
+                }
 
                 return Math.Max(leftDepth, rightDepth) + 1;
             }
-            int leftDepth = GetDept(root.left);
-            int rightDepth = GetDept(root.right);
-            return Math.Abs(leftDepth - rightDepth) <= 1;
+
+            return GetDepth(root) != -1;
         }
     }
 }
